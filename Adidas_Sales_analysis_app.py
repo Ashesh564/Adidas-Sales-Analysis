58e93c6b10c586d97ed7a6a4c79cd336f2be0d29
Ashesh Kalyani <76043005+Ashesh564@users.noreply.github.com>
2024-03-29 00:06:44 +0530

    Update Adidas_Sales_analysis_app.py

--- Adidas_Sales_analysis_app.py
+++ Adidas_Sales_analysis_app.py
@@ -31,7 +31,7 @@ html_title = """
     border-radius:6px;
     }
     </style>
-    <center><h1 class="title-test">Adidas Interactive Sales Dashboard</h1></center>"""
+    <center><h1 class="title-test">Adidas Sales Dashboard</h1></center>"""
 with col2:
     st.markdown(html_title, unsafe_allow_html=True)
 
